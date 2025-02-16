5d9658ac4a03f546b25cf27376f3f8b1370482d0
Heather-meatherall <90055263+Heather-meatherall@users.noreply.github.com>
2025-02-16 13:28:32 -0500

    createAccount varribles updated

--- Phase-2-Frontend/createAccount.py
+++ Phase-2-Frontend/createAccount.py
@@ -1,45 +1,40 @@
-nextAccount = 0
+next_account = 0
 
 class createAccount:
-    accountHolder = ''
-    accountNumber = ''
-    initialBalance = 0
-    paymentPlan = 'SP'
-    transactionFileLine = ''
-    accountInfo = ''
+    account_holder = ''
+    account_number = ''
+    initial_balance = 0
+    payment_plan = 'SP'
+    transaction_file_line = ''
+    account_info = ''
 
     def create(self):
-        global nextAccount
+        global next_account
 
-        self.accountHolder = input("enter account holder ").ljust(20)
-        self.initialBalance = input("enter an initial balance ").zfill(8)
-        nextAccount += 1
-        self.accountNumber = str(nextAccount).zfill(5)
+        self.account_holder = input("enter account holder ").ljust(20)
+        self.initial_balance = input("enter an initial balance ").zfill(8)
+        next_account += 1
+        self.account_number = str(next_account).zfill(5)
 
-        self.transactionFileLine = "05 " + self.accountHolder + " " + self.accountNumber + " " + self.initialBalance+ " AA"
-        self.accountInfo = self.accountNumber + ' ' + self.accountHolder + ' A ' + self.initialBalance
+        self.transaction_file_line = "05 " + self.account_holder + " " + self.account_number + " " + self.initial_balance+ " AA"
+        self.account_info = self.account_number + ' ' + self.account_holder + ' A ' + self.initial_balance
 
 
         #add to current account file
         #add transaction line to file
-        print(f'account {self.accountNumber} was created with initial balance {self.initialBalance}')
-        return self.transactionFileLine
+        print(f'account {self.account_number} was created with initial balance {self.initial_balance}')
+        return self.transaction_file_line
 
     def changeplan(self):
-        self.accountHolder = input("enter account holder ")
-        self.accountNumber = input("enter account number ")
-        self.paymentPlan = 'NP'
+        self.account_holder = input("enter account holder ")
+        self.account_number = input("enter account number ")
+        self.payment_plan = 'NP'
 
         #get balance from account file
         #update to current account file
         #add transaction line to file
-        self.transactionFileLine = "08 " + str(self.accountHolder).ljust(20) + " " + self.accountNumber + " " + str(self.initialBalance).zfill(8) + " " + self.paymentPlan
-        self.accountInfo = self.accountNumber + ' ' + self.accountHolder + ' A ' + str(self.initialBalance)
+        self.transaction_file_line = "08 " + str(self.account_holder).ljust(20) + " " + self.account_number + " " + str(self.initial_balance).zfill(8) + " " + self.payment_plan
+        self.account_info = self.account_number + ' ' + self.account_holder + ' A ' + str(self.initial_balance)
         
-        print(f'account {self.accountNumber} payment plan changed to non-student')
-        return self.transactionFileLine
-
-
-# p1 = createAccount()
-# # p1.createAccount()
-# p1.changePlan()
+        print(f'account {self.account_number} payment plan changed to non-student')
+        return self.transaction_file_line
