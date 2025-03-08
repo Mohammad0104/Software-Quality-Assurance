6545ca57ca64b858b075dadb1b5cf7d20eb4e7c1
Mohammad <mohammad1@ontariotechu.net>
2025-03-08 15:03:30 -0500

    create_account

--- Phase-2-Frontend/createAccount.py
+++ Phase-2-Frontend/createAccount.py
@@ -1,40 +1,36 @@
-next_account = 0
+class CreateAccount:
+    def __init__(self, account_file='accounts.txt'):
+        """Initialize the class with the path to the accounts file and set the next account number."""
+        self.account_file = account_file
+        self.next_account_number = self.get_next_account_number()
 
-class createAccount:
-    account_holder = ''
-    account_number = ''
-    initial_balance = 0
-    payment_plan = 'SP'
-    transaction_file_line = ''
-    account_info = ''
+    def get_next_account_number(self):
+        """Determines the next account number by reading the last entry in the accounts file."""
+        try:
+            with open(self.account_file, 'r') as file:
+                lines = file.readlines()
+                if lines and lines[-1].startswith('END_OF_FILE'):
+                    last_account_number = int(lines[-2].split()[0])
+                else:
+                    last_account_number = int(lines[-1].split()[0]) if lines else 0
+                return last_account_number + 1
+        except FileNotFoundError:
+            return 1
 
     def create(self):
-        global next_account
+        """Prompts user input for account details, creates a new account, and appends it to the accounts file."""
+        account_holder = input("Enter account holder: ").ljust(20)
+        initial_balance = input("Enter an initial balance: ").zfill(8)
+        account_number = str(self.next_account_number).zfill(5)
+        account_info = f"{account_number} {account_holder} A {initial_balance}"
 
-        self.account_holder = input("enter account holder ").ljust(20)
-        self.initial_balance = input("enter an initial balance ").zfill(8)
-        next_account += 1
-        self.account_number = str(next_account).zfill(5)
+        with open(self.account_file, 'a') as file:
+            file.write(account_info + '\n')
 
-        self.transaction_file_line = "05 " + self.account_holder + " " + self.account_number + " " + self.initial_balance+ " AA"
-        self.account_info = self.account_number + ' ' + self.account_holder + ' A ' + self.initial_balance
+        print(f'Account {account_number} was created with initial balance {initial_balance}')
+        self.next_account_number += 1  
+        return account_info
 
-
-        #add to current account file
-        #add transaction line to file
-        print(f'account {self.account_number} was created with initial balance {self.initial_balance}')
-        return self.transaction_file_line
-
-    def change_plan(self):
-        self.account_holder = input("enter account holder ")
-        self.account_number = input("enter account number ")
-        self.payment_plan = 'NP'
-
-        #get balance from account file
-        #update to current account file
-        #add transaction line to file
-        self.transaction_file_line = "08 " + str(self.account_holder).ljust(20) + " " + self.account_number + " " + str(self.initial_balance).zfill(8) + " " + self.payment_plan
-        self.account_info = self.account_number + ' ' + self.account_holder + ' A ' + str(self.initial_balance)
-        
-        print(f'account {self.account_number} status has been change to NP')
-        return self.transaction_file_line
+# Create an instance and add a new account
+createAccount = CreateAccount()
+createAccount.create()
