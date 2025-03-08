7ac91db688088476ea3cc86dd93f62c0e5e33fbc
Heather-meatherall <90055263+Heather-meatherall@users.noreply.github.com>
2025-03-07 20:01:59 -0500

    added ability to read and update account file

--- Phase-2-Frontend/moveMoney.py
+++ Phase-2-Frontend/moveMoney.py
@@ -1,3 +1,5 @@
+import account_utils
+
 class moveMoney:
     account_holder = ''
     account_number = 0
@@ -5,21 +7,29 @@ class moveMoney:
     
     
     def transfer(self):
+        accounts = account_utils.read_bank_accounts()
         transferTo = 0
         transferFrom = 0
         transferAmount = 0.0
 
         amountCheck = False
     
-        #User enters account holder
+        #Get account holder name and find the account
         self.account_holder = input("Enter account holder ")
+        account = account_utils.find_account(accounts, self.account_holder)
         
 
         #User enters account number whom they want money transferred to/from
         self.transferTo = input("enter account number to transfer to ")
+        if self.transferTo != account["account_number"]:
+            print("Error: Account number does not match account holder.")
+            return None
         self.transferFrom = input("enter account number to transfer from ")
+        if self.transferFrom != account["account_number"]:
+            print("Error: Account number does not match account holder.")
+            return None
 
-        print(f"balance value: {self.balance}")
+        print(f"balance value: {account['balance']}")
 
         #Checks if amount inputted is within limit
         while (not amountCheck):
@@ -37,6 +47,12 @@ class moveMoney:
             
             else:
                 amountCheck = True
+        
+        for account in accounts:
+            if account["account_name"].lower() == self.transferTo.lower():
+                account["balance"] = account["balance"] + float(self.transferTo)
+            if account["account_name"].lower() == self.transferFrom.lower():
+                account["balance"] = account["balance"] + float(self.transferFrom)
 
         print("Transfer accepted")
 
@@ -44,6 +60,7 @@ class moveMoney:
 
 
     def pay_bill(self):
+        accounts = account_utils.read_bank_accounts()
         company = ''
         companyCheck = False
         payAmount = 0
@@ -52,9 +69,13 @@ class moveMoney:
 
         #User enters account holder
         self.account_holder = input("Enter account holder ")
+        account = account_utils.find_account(accounts, self.account_holder)
         
         #User enters account number
         self.account_number = input("Enter account number ")
+        if self.account_number != account["account_number"]:
+            print("Error: Account number does not match account holder.")
+            return None
 
         #Checks if company inputted is on the list
         while (not companyCheck):
@@ -86,22 +107,41 @@ class moveMoney:
             
             else:
                 amountCheck = True
+            
+        for account in accounts:
+            if account["account_name"].lower() == self.account_holder.lower():
+                account["balance"] = account["balance"] - float(self.payAmount)
 
         print(f'{self.payAmount} bill sent to account {self.account_number}')
+
+        
+        account_utils.update_bank_accounts(accounts)
 
 
 
 
     def deposit(self):
-
+        accounts = account_utils.read_bank_accounts()
         depositAmount = 0.0
 
         #User enters account holder
         self.account_holder = input("enter account holder ")
+        account = account_utils.find_account(accounts, self.account_holder)
+        if not account:
+            print('Error: User name does not exist.')
+            return None
 
         #User enters account number
         self.account_number = input("enter account number ")
+        if self.account_number != account["account_number"]:
+            print("Error: Account number does not match account holder.")
+            return None
 
         self.depositAmount = input("enter amount to deposit ")
 
+        for account in accounts:
+            if account["account_name"].lower() == self.account_holder.lower():
+                account["balance"] = account["balance"] + float(self.depositAmount)
+
         print(f'{self.depositAmount} deposited into account {self.account_number}')
+        account_utils.update_bank_accounts(accounts)
